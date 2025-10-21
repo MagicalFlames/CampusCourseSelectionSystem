e003d0b1cc9a1d1d873f94aaff02f42e9e2b7083
MagicalFlames <3398455006@qq.com>
2025-10-21 16:56:48 +0800

    Initial commit

---
+++ src/main/java/com/zjgsu/wzy/campuscourseselectionsystem/CampusCourseSelectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zjgsu.wzy.campuscourseselectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCourseSelectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusCourseSelectionSystemApplication.class, args);
+    }
+
+}
